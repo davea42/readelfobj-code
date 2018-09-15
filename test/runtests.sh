5603ace00359b3ee76f15e78c63ffaf85747c3ff
David Anderson <davea42@linuxmail.org>
2018-09-14 22:04:07 -0700

    Run the set of tests 	new file:   test/runtests.sh

---
+++ test/runtests.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+if [ ! -x readelfobj ]
+then
+   echo "Build readelfobj and install it here to test"
+   echo "No test done"
+   exit 1
+fi
+
+fail=0
+for i in test[0-9]*.sh
+do
+  sh $i
+  if [ $? -ne 0 ]
+  then
+     ct=`expr $ct + 1`  
+  fi
+done
+
+if [ $ct -gt 0 ]
+then
+  echo "FAIL COUNT: $ct"
+  exit 1
+fi
+exit 0
