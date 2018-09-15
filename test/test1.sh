af22371b10b051c75bb065ba311ddd2593395718
David Anderson <davea42@linuxmail.org>
2018-09-14 18:40:00 -0700

    First test case and some superstructure. 	new file:   CLEANUP 	new file:   PICKUP 	new file:   README 	new file:   libkrb5support.so.0.1.debug 	new file:   test1.base 	new file:   test1.sh

---
+++ test/test1.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+n=test1
+base=$n.base
+o=libkrb5support.so.0.1.debug
+#echo "START test $n "
+./readelfobj --help $o  >junk.$n
+diff junk.$n $n.base > junk.$n.out
+if [ $? -ne 0 ]
+then
+  cat junk.$n.out
+  echo "FAIL $n.sh results differ $n.base vs junk.$n.out"
+  echo "To update, mv junk.$n $n.base"
+  exit 1
+fi
+exit 0
+
