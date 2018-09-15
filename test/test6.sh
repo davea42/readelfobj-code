d8c944ba341b9f37c7ff5de799b691156cba3721
David Anderson <davea42@linuxmail.org>
2018-09-14 19:11:21 -0700

    Basic test set. 	new file:   test3.base 	new file:   test3.sh 	new file:   test4.base 	new file:   test4.sh 	new file:   test5.base 	new file:   test5.sh 	new file:   test6.base 	new file:   test6.sh 	new file:   test7.base 	new file:   test7.sh

---
+++ test/test6.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+n=test6
+base=$n.base
+o=libkrb5support.so.0.1.debug
+cmd="--print-wasted"
+#echo "START test $n "
+./readelfobj $cmd $o  >junk.$n
+diff $n.base junk.$n > junk.$n.out
+if [ $? -ne 0 ]
+then
+  head -30 junk.$n.out
+  echo "FAIL $n.sh $cmd, results differ $n.base vs junk.$n.out"
+  echo "To update, mv junk.$n $n.base"
+  exit 1
+fi
+exit 0
+
