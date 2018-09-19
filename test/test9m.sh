1d270131510540d8769189cd03aef847a5377da5
David Anderson <davea42@linuxmail.org>
2018-09-19 11:34:41 -0700

    Testing readobjmacho 	modified:   test/PICKUP 	new file:   test/test9m.base 	new file:   test/test9m.sh

---
+++ test/test9m.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+n=test9m
+base=$n.base
+o="mach-o-object32 mach-o-object64 libkrb5support.so.0.1.debug"
+#echo "START test $n "
+./readobjmacho  $o  >junk.$n
+diff $n.base junk.$n > junk.$n.out
+if [ $? -ne 0 ]
+then
+  head -30 junk.$n.out
+  echo "FAIL $n.sh $cmd, results differ $n.base vs junk.$n.out"
+  echo "To update, mv junk.$n $n.base"
+  exit 1
+fi
+exit 0
+
