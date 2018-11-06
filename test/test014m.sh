461881650d1743b7c1eb4b7e5db9bc6c45acbf8f
David Anderson <davea42@linuxmail.org>
2018-11-06 07:03:33 -0800

    version 20181105 	modified:   configure.ac
    Fixed the pe and mach-o endianness tests, which
    were somewhat broken.
    	modified:   src/dwarf_object_detector.c
    
    corrected tests, added new one.
    	modified:   test/Makefile.am
    	modified:   test/test008m.base
    	new file:   test/test014m.base
    	new file:   test/test014m.sh

---
+++ test/test014m.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+n=test014m
+base=$n.base
+# This test should fail on all as we don't allow outpath
+# in the dwarf_object_detector_path call.
+o=kask2/dwarfdump_G4
+#echo "START test $n "
+./object_detector  $o  >junk.$n
+diff $n.base junk.$n > junk.$n.out
+if [ $? -ne 0 ]
+then
+  head -30 junk.$n.out
+  echo "FAIL $n.sh $cmd, results differ $n.base vs junk.$n.out"
+  echo "To update, mv junk.$n $n.base"
+  exit 1
+fi
+exit 0
+
