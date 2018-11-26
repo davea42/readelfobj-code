b7c23c783e24faf9a656d59e72b2ad244c476f97
David Anderson <davea42@linuxmail.org>
2018-11-26 09:17:33 -0800

    PE tests 	new file:   test/test015p.base 	new file:   test/test015p.sh 	new file:   test/test016p.base 	new file:   test/test016p.sh

---
+++ test/test016p.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+n=test015m
+base=$n.base
+o=libexamine-0.dll
+#echo "START test $n "
+./readobjpe  $o  >junk.$n
+diff $n.base junk.$n > junk.$n.out
+if [ $? -ne 0 ]
+then
+  head -30 junk.$n.out
+  echo "FAIL $n.sh $cmd, results differ $n.base vs junk.$n.out"
+  echo "To update, mv junk.$n $n.base"
+  exit 1
+fi
+exit 0
+
