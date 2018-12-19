5f61ebb36f3dd2b5a62ad0caa68e33b5fcf4472c
David Anderson <davea42@linuxmail.org>
2018-12-19 08:51:47 -0800

    modified:   configure.ac version 20181219
    Add newest tests to the files lists
    	modified:   test/Makefile.am
    
    New test case  64bit PE object
    	new file:   test/kask-dwarfdump_64.exe
    	new file:   test/test018p.base
    	new file:   test/test018p.sh

---
+++ test/test018p.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+n=test018p
+base=$n.base
+o=kask-dwarfdump_64.exe
+#echo "START test $n "
+./readobjpe  $o  >junk.$n
+dos2unix -q junk.$n
+diff $n.base junk.$n > junk.$n.out
+if [ $? -ne 0 ]
+then
+  head -30 junk.$n.out
+  echo "FAIL $n.sh $cmd, results differ $n.base vs junk.$n.out"
+  echo "To update, mv junk.$n $n.base"
+  exit 1
+fi
+exit 0
+
