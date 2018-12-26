f43717bad9150bc49c04b9756c1182ef0f6f025b
David Anderson <davea42@linuxmail.org>
2018-12-26 09:16:12 -0800

    Removing unwanted comments. 	modified:   raw_elf_definesdwarf_reloc_ppc.h 	modified:   raw_elf_definesdwarf_reloc_ppc64.h 	modified:   raw_elf_definesdwarf_reloc_x86_64.h

--- scripts/raw_elf_definesdwarf_reloc_ppc64.h
+++ scripts/raw_elf_definesdwarf_reloc_ppc64.h
@@ -1,16 +1,16 @@
 /* defines needed for interoperability.  */
 #define R_PPC64_NONE                R_PPC_NONE
-#define R_PPC64_ADDR32              R_PPC_ADDR32 /* 32bit absolute address.  */
-#define R_PPC64_ADDR24              R_PPC_ADDR24 /* 26bit address, word aligned.  */
-#define R_PPC64_ADDR16              R_PPC_ADDR16 /* 16bit absolute address. */
-#define R_PPC64_ADDR16_LO           R_PPC_ADDR16_LO /* lower 16bits of abs. address.  */
-#define R_PPC64_ADDR16_HI           R_PPC_ADDR16_HI /* high 16bits of abs. address. */
-#define R_PPC64_ADDR16_HA           R_PPC_ADDR16_HA /* adjusted high 16bits.  */
-#define R_PPC64_ADDR14              R_PPC_ADDR14  /* 16bit address, word aligned.  */
+#define R_PPC64_ADDR32              R_PPC_ADDR32
+#define R_PPC64_ADDR24              R_PPC_ADDR24
+#define R_PPC64_ADDR16              R_PPC_ADDR16
+#define R_PPC64_ADDR16_LO           R_PPC_ADDR16_LO
+#define R_PPC64_ADDR16_HI           R_PPC_ADDR16_HI
+#define R_PPC64_ADDR16_HA           R_PPC_ADDR16_HA
+#define R_PPC64_ADDR14              R_PPC_ADDR14
 #define R_PPC64_ADDR14_BRTAKEN      R_PPC_ADDR14_BRTAKEN
 #define R_PPC64_ADDR14_BRNTAKEN     R_PPC_ADDR14_BRNTAKEN
-#define R_PPC64_REL24               R_PPC_REL24 /* PC relative 26 bit, word aligned.  */
-#define R_PPC64_REL14               R_PPC_REL14 /* PC relative 16 bit. */
+#define R_PPC64_REL24               R_PPC_REL24
+#define R_PPC64_REL14               R_PPC_REL14
 #define R_PPC64_REL14_BRTAKEN       R_PPC_REL14_BRTAKEN
 #define R_PPC64_REL14_BRNTAKEN      R_PPC_REL14_BRNTAKEN
 #define R_PPC64_GOT16               R_PPC_GOT16
@@ -33,76 +33,76 @@
 #define R_PPC64_SECTOFF_LO          R_PPC_SECTOFF_LO
 #define R_PPC64_SECTOFF_HI          R_PPC_SECTOFF_HI
 #define R_PPC64_SECTOFF_HA          R_PPC_SECTOFF_HA
-#define R_PPC64_ADDR30              37 /* word30 (S + A - P) >> 2.  */
-#define R_PPC64_ADDR64              38 /* doubleword64 S + A.  */
-#define R_PPC64_ADDR16_HIGHER       39 /* half16 #higher(S + A).  */
-#define R_PPC64_ADDR16_HIGHERA      40 /* half16 #highera(S + A).  */
-#define R_PPC64_ADDR16_HIGHEST      41 /* half16 #highest(S + A).  */
-#define R_PPC64_ADDR16_HIGHESTA     42 /* half16 #highesta(S + A). */
-#define R_PPC64_UADDR64             43 /* doubleword64 S + A.  */
-#define R_PPC64_REL64               44 /* doubleword64 S + A - P.  */
-#define R_PPC64_PLT64               45 /* doubleword64 L + A.  */
-#define R_PPC64_PLTREL64            46 /* doubleword64 L + A - P.  */
-#define R_PPC64_TOC16               47 /* half16* S + A - .TOC.  */
-#define R_PPC64_TOC16_LO            48 /* half16 #lo(S + A - .TOC.).  */
-#define R_PPC64_TOC16_HI            49 /* half16 #hi(S + A - .TOC.).  */
-#define R_PPC64_TOC16_HA            50 /* half16 #ha(S + A - .TOC.).  */
-#define R_PPC64_TOC                 51 /* doubleword64 .TOC. */
-#define R_PPC64_PLTGOT16            52 /* half16* M + A.  */
-#define R_PPC64_PLTGOT16_LO         53 /* half16 #lo(M + A).  */
-#define R_PPC64_PLTGOT16_HI         54 /* half16 #hi(M + A).  */
-#define R_PPC64_PLTGOT16_HA         55 /* half16 #ha(M + A).  */
-#define R_PPC64_ADDR16_DS           56 /* half16ds* (S + A) >> 2.  */
-#define R_PPC64_ADDR16_LO_DS        57 /* half16ds  #lo(S + A) >> 2.  */
-#define R_PPC64_GOT16_DS            58 /* half16ds* (G + A) >> 2.  */
-#define R_PPC64_GOT16_LO_DS         59 /* half16ds  #lo(G + A) >> 2.  */
-#define R_PPC64_PLT16_LO_DS         60 /* half16ds  #lo(L + A) >> 2.  */
-#define R_PPC64_SECTOFF_DS          61 /* half16ds* (R + A) >> 2.  */
-#define R_PPC64_SECTOFF_LO_DS       62 /* half16ds  #lo(R + A) >> 2.  */
-#define R_PPC64_TOC16_DS            63 /* half16ds* (S + A - .TOC.) >> 2.  */
-#define R_PPC64_TOC16_LO_DS         64 /* half16ds  #lo(S + A - .TOC.) >> 2.  */
-#define R_PPC64_PLTGOT16_DS         65 /* half16ds* (M + A) >> 2.  */
-#define R_PPC64_PLTGOT16_LO_DS      66 /* half16ds  #lo(M + A) >> 2.  */
-#define R_PPC64_TLS                 67 /* none      (sym+add)@tls */
-#define R_PPC64_DTPMOD64            68 /* doubleword64 (sym+add)@dtpmod */
-#define R_PPC64_TPREL16             69 /* half16*   (sym+add)@tprel */
-#define R_PPC64_TPREL16_LO          70 /* half16    (sym+add)@tprel@l */
-#define R_PPC64_TPREL16_HI          71 /* half16    (sym+add)@tprel@h */
-#define R_PPC64_TPREL16_HA          72 /* half16    (sym+add)@tprel@ha */
-#define R_PPC64_TPREL64             73 /* doubleword64 (sym+add)@tprel */
-#define R_PPC64_DTPREL16            74 /* half16*   (sym+add)@dtprel */
-#define R_PPC64_DTPREL16_LO         75 /* half16    (sym+add)@dtprel@l */
-#define R_PPC64_DTPREL16_HI         76 /* half16    (sym+add)@dtprel@h */
-#define R_PPC64_DTPREL16_HA         77 /* half16    (sym+add)@dtprel@ha */
-#define R_PPC64_DTPREL64            78 /* doubleword64 (sym+add)@dtprel */
-#define R_PPC64_GOT_TLSGD16         79 /* half16*   (sym+add)@got@tlsgd */
-#define R_PPC64_GOT_TLSGD16_LO      80 /* half16    (sym+add)@got@tlsgd@l */
-#define R_PPC64_GOT_TLSGD16_HI      81 /* half16    (sym+add)@got@tlsgd@h */
-#define R_PPC64_GOT_TLSGD16_HA      82 /* half16    (sym+add)@got@tlsgd@ha */
-#define R_PPC64_GOT_TLSLD16         83 /* half16*   (sym+add)@got@tlsld */
-#define R_PPC64_GOT_TLSLD16_LO      84 /* half16    (sym+add)@got@tlsld@l */
-#define R_PPC64_GOT_TLSLD16_HI      85 /* half16    (sym+add)@got@tlsld@h */
-#define R_PPC64_GOT_TLSLD16_HA      86 /* half16    (sym+add)@got@tlsld@ha */
-#define R_PPC64_GOT_TPREL16_DS      87 /* half16ds* (sym+add)@got@tprel */
-#define R_PPC64_GOT_TPREL16_LO_DS   88 /* half16ds (sym+add)@got@tprel@l */
-#define R_PPC64_GOT_TPREL16_HI      89 /* half16    (sym+add)@got@tprel@h */
-#define R_PPC64_GOT_TPREL16_HA      90 /* half16    (sym+add)@got@tprel@ha */
-#define R_PPC64_GOT_DTPREL16_DS     91 /* half16ds* (sym+add)@got@dtprel */
-#define R_PPC64_GOT_DTPREL16_LO_DS  92 /* half16ds (sym+add)@got@dtprel@l */
-#define R_PPC64_GOT_DTPREL16_HI     93 /* half16    (sym+add)@got@dtprel@h */
-#define R_PPC64_GOT_DTPREL16_HA     94 /* half16    (sym+add)@got@dtprel@ha */
-#define R_PPC64_TPREL16_DS          95 /* half16ds* (sym+add)@tprel */
-#define R_PPC64_TPREL16_LO_DS       96 /* half16ds  (sym+add)@tprel@l */
-#define R_PPC64_TPREL16_HIGHER      97 /* half16    (sym+add)@tprel@higher */
-#define R_PPC64_TPREL16_HIGHERA     98 /* half16    (sym+add)@tprel@highera */
-#define R_PPC64_TPREL16_HIGHEST     99 /* half16    (sym+add)@tprel@highest */
-#define R_PPC64_TPREL16_HIGHESTA   100 /* half16  (sym+add)@tprel@highesta */
-#define R_PPC64_DTPREL16_DS        101 /* half16ds* (sym+add)@dtprel */
-#define R_PPC64_DTPREL16_LO_DS     102 /* half16ds (sym+add)@dtprel@l */
-#define R_PPC64_DTPREL16_HIGHER    103 /* half16   (sym+add)@dtprel@higher */
-#define R_PPC64_DTPREL16_HIGHERA   104 /* half16  (sym+add)@dtprel@highera */
-#define R_PPC64_DTPREL16_HIGHEST   105 /* half16  (sym+add)@dtprel@highest */
-#define R_PPC64_DTPREL16_HIGHESTA  106 /* half16 (sym+add)@dtprel@highesta */
+#define R_PPC64_ADDR30              37
+#define R_PPC64_ADDR64              38
+#define R_PPC64_ADDR16_HIGHER       39
+#define R_PPC64_ADDR16_HIGHERA      40
+#define R_PPC64_ADDR16_HIGHEST      41
+#define R_PPC64_ADDR16_HIGHESTA     42
+#define R_PPC64_UADDR64             43
+#define R_PPC64_REL64               44
+#define R_PPC64_PLT64               45
+#define R_PPC64_PLTREL64            46
+#define R_PPC64_TOC16               47
+#define R_PPC64_TOC16_LO            48
+#define R_PPC64_TOC16_HI            49
+#define R_PPC64_TOC16_HA            50
+#define R_PPC64_TOC                 51
+#define R_PPC64_PLTGOT16            52
+#define R_PPC64_PLTGOT16_LO         53
+#define R_PPC64_PLTGOT16_HI         54
+#define R_PPC64_PLTGOT16_HA         55
+#define R_PPC64_ADDR16_DS           56
+#define R_PPC64_ADDR16_LO_DS        57
+#define R_PPC64_GOT16_DS            58
+#define R_PPC64_GOT16_LO_DS         59
+#define R_PPC64_PLT16_LO_DS         60
+#define R_PPC64_SECTOFF_DS          61
+#define R_PPC64_SECTOFF_LO_DS       62
+#define R_PPC64_TOC16_DS            63
+#define R_PPC64_TOC16_LO_DS         64
+#define R_PPC64_PLTGOT16_DS         65
+#define R_PPC64_PLTGOT16_LO_DS      66
+#define R_PPC64_TLS                 67
+#define R_PPC64_DTPMOD64            68
+#define R_PPC64_TPREL16             69
+#define R_PPC64_TPREL16_LO          70
+#define R_PPC64_TPREL16_HI          71
+#define R_PPC64_TPREL16_HA          72
+#define R_PPC64_TPREL64             73
+#define R_PPC64_DTPREL16            74
+#define R_PPC64_DTPREL16_LO         75
+#define R_PPC64_DTPREL16_HI         76
+#define R_PPC64_DTPREL16_HA         77
+#define R_PPC64_DTPREL64            78
+#define R_PPC64_GOT_TLSGD16         79
+#define R_PPC64_GOT_TLSGD16_LO      80
+#define R_PPC64_GOT_TLSGD16_HI      81
+#define R_PPC64_GOT_TLSGD16_HA      82
+#define R_PPC64_GOT_TLSLD16         83
+#define R_PPC64_GOT_TLSLD16_LO      84
+#define R_PPC64_GOT_TLSLD16_HI      85
+#define R_PPC64_GOT_TLSLD16_HA      86
+#define R_PPC64_GOT_TPREL16_DS      87
+#define R_PPC64_GOT_TPREL16_LO_DS   88
+#define R_PPC64_GOT_TPREL16_HI      89
+#define R_PPC64_GOT_TPREL16_HA      90
+#define R_PPC64_GOT_DTPREL16_DS     91
+#define R_PPC64_GOT_DTPREL16_LO_DS  92
+#define R_PPC64_GOT_DTPREL16_HI     93
+#define R_PPC64_GOT_DTPREL16_HA     94
+#define R_PPC64_TPREL16_DS          95
+#define R_PPC64_TPREL16_LO_DS       96
+#define R_PPC64_TPREL16_HIGHER      97
+#define R_PPC64_TPREL16_HIGHERA     98
+#define R_PPC64_TPREL16_HIGHEST     99
+#define R_PPC64_TPREL16_HIGHESTA   100
+#define R_PPC64_DTPREL16_DS        101
+#define R_PPC64_DTPREL16_LO_DS     102
+#define R_PPC64_DTPREL16_HIGHER    103
+#define R_PPC64_DTPREL16_HIGHERA   104
+#define R_PPC64_DTPREL16_HIGHEST   105
+#define R_PPC64_DTPREL16_HIGHESTA  106
 #define R_PPC64_TOC32              107
 #define R_PPC64_DTPMOD32           108
 #define R_PPC64_TPREL32            109
