5bc33338000e864b2d10f38a70e65c29bc9b871a
David Anderson <davea42@linuxmail.org>
2019-10-17 15:50:09 -0700

    In test/:  * Makefile.am,runtests.sh,PICKUP: Small changes so make check       works when run out=of=sourcetree.  * Makefile.in: regenerated.

--- test/runtests.sh
+++ test/runtests.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-ct=0
-if [ ! -x readelfobj ]
-then
-   echo "Build readelfobj and install it here to test"
-   echo "No test done"
-   exit 1
-fi
 top_blddir=`pwd`/..
 if [ x$DWTOPSRCDIR = "x" ]
 then
@@ -15,6 +8,20 @@ else
   top_srcdir=$DWTOPSRCDIR
 fi
 srcdir=$top_srcdir/test
+loc=`pwd`
+ct=0
+if [ ! -x $top_blddir/src/readelfobj ]
+then
+   echo "Build readelfobj and install it as $loc/../src/readelfobj to test"
+   echo "No test done"
+   exit 1
+fi
+sh $top_srcdir/test/PICKUP $top_blddir
+if [ $? -ne 0 ]
+then
+  echo "FAIL executing $top_srcdir/test/PICKUP in test/runtest.sh"
+  exit 1
+fi
 . $top_srcdir/SHALIAS.sh
 fail=0
 for i in $srcdir/test[0-9]*.sh
