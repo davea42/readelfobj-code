568e966abdc22056fb80dd1708f46deaafe52060
David Anderson <davea42@linuxmail.org>
2023-04-03 13:50:55 -0700

    these all generate object files using the documented way of getting 32 or 64 bit section count and a shstrindex of 32 bits  in section headers. 	new file:   README 	new file:   elfextendedtestcase.c 	new file:   patcher.sh 	new file:   testobj.baseobj 	new file:   testobj.extend 	new file:   testobj64.baseobj 	new file:   testobj64.extend

---
+++ test/elfextended/elfextendedtestcase.c
@@ -0,0 +1,24 @@
+/*   Copyright (c) 2023 David Anderson
+    This trivial test code is hereby placed in the public domain
+    for anyone to use for any purpose. */
+
+/*  Not to be compiled. This test case should remain unchanged.
+    Compile with gcc -gdwarf-5 -O0 elfextendedtestcase.c -o testobj
+
+    Compile with gcc -gdwarf-5 -m64 -O0 elfextendedtestcase.c -o testobj64
+*/
+#if 1
+#include "stdio.h"
+
+int main()
+{
+    int i = 0;
+    int max = 5;
+    int sum = 0;
+
+    for ( ; i < max; ++i) {
+        sum += i;
+    }
+    return sum;
+}
+#endif
