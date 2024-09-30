431fb7fe326f128a41fbe1dc1b81338ea0dabe19
David Anderson <davea42@linuxmail.org>
2024-09-30 16:42:05 -0700

    new tests for macho object reporting. 	new file:   test/test031m.base 	new file:   test/test031m.sh 	new file:   test/test032m.base 	new file:   test/test032m.sh

---
+++ test/test032m.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+n=test032m
+if [ x$DWTOPSRCDIR = "x" ]
+then
+  top_srcdir=$top_blddir
+else
+  top_srcdir=$DWTOPSRCDIR
+fi
+srcdir=$top_srcdir/test
+df=$srcdir/testdiff.py
+base=$srcdir/$n.base
+curdir=`pwd`
+
+o="$srcdir/mach-o-object32 $srcdir/mach-o-object64 $srcdir/libkrb5support.so.0.1.debug"
+x="../src/readobjmacho --sections-by-name $o"
+echo "START $n $x"
+$x > junk.$n.tmp
+
+$df  $base junk.$n.tmp "$srcdir" > junk.$n.out
+if [ $? -ne 0 ]
+then
+  head -30 junk.$n.out
+  echo "FAIL $n.sh $cmd, results differ $base vs junk.$n.tmp"
+  echo "To update, cp $curdir/junk.$n.tmp $base"
+  exit 1
+fi
+exit 0
+
