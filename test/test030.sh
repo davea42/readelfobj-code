a3c0a36c9423b5f87db43f9437b9518b93656776
David Anderson <davea42@linuxmail.org>
2024-09-30 14:14:40 -0700

    Testing the new readelfobj options:  --sections-by-size  --sections-by-name If neither presented the sections are listed as presented in Elf order. 	new file:   test/test029.base 	new file:   test/test029.sh 	new file:   test/test030.base 	new file:   test/test030.sh

---
+++ test/test030.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+n=test030
+if [ x$DWTOPSRCDIR = "x" ]
+then
+  top_srcdir=$top_blddir
+else
+  top_srcdir=$DWTOPSRCDIR
+fi
+srcdir=$top_srcdir/test
+df=$srcdir/testdiff.py
+base=$srcdir/$n.base
+curdir=`pwd`
+o=$srcdir/libkrb5support.so.0.1.debug
+cmd="--sections-by-name"
+x="../src/readelfobj $cmd $o"
+echo "START $n $x"
+$x > junk.$n.tmp
+
+$df  $base junk.$n.tmp "$srcdif" > junk.$n.out
+if [ $? -ne 0 ]
+then
+  cat junk.$n.out
+  echo "FAIL $n.sh results differ $n.base vs junk.$n.tmp"
+  echo "To update, cp $curdir/junk.$n.tmp $base"
+  exit 1
+fi
+exit 0
+
