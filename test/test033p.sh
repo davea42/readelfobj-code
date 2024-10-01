95c49ad0631387d330aeb759ab83768b13f59266
David Anderson <davea42@linuxmail.org>
2024-10-01 11:17:04 -0700

    pe tests of --sections-by-name sections-by-size 	new file:   test033p.base 	new file:   test033p.sh 	new file:   test034p.base 	new file:   test034p.sh

---
+++ test/test033p.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+n=test033p
+if [ x$DWTOPSRCDIR = "x" ]
+then
+  top_srcdir=$top_blddir
+else
+  top_srcdir=$DWTOPSRCDIR
+fi
+srcdir=$top_srcdir/test
+df=$srcdir/testdiff.py
+base=$srcdir/$n.base
+o=$srcdir/libexamine-0.dll
+curdir=`pwd`
+
+x="../src/readobjpe --sections-by-size  $o"
+echo "START $n $x"
+$x > junk.$n.tmp
+# Emits junk.$n.tmpforbase
+nin=junk.$n.tmp
+nout=junk.$n.tmpforbase
+$df  $base $nin "$srcdir" > junk.$n.out
+if [ $? -ne 0 ]
+then
+  cat junk.$n.out
+  echo "FAIL $n.sh results differ $base vs $nout"
+  echo "To update, cp $curdir/$nout $base"
+  exit 1
+fi
+exit 0
+
